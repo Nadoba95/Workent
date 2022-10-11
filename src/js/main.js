e3cdc847740704884356567e43960e2163094c8f
Dejan Nadoba <dejan.nadoba@vega.local>
2022-10-11 16:48:37 +0200

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const btnHeaderMenu = document.querySelector(".js-header-btn-menu");
+const btnHeaderDropdown = document.querySelector(".js-header-btn-dropdown");
+const headerNav = document.querySelector(".js-header-nav");
+const header = document.querySelector(".js-header");
+
+function toggleMenu() {
+  const iconMenu = this.querySelector(".js-header-icon-menu");
+  const iconClose = this.querySelector(".js-header-icon-close");
+
+  iconMenu.classList.toggle("hidden");
+  iconClose.classList.toggle("hidden");
+  this.classList.toggle("active");
+
+  if (this.classList.contains("active")) {
+    header.classList.add("active");
+    headerNav.style.display = "block";
+  } else {
+    header.classList.remove("active");
+    headerNav.style.display = "none";
+  }
+}
+
+function toggleDropdown() {
+  const dropdownItem = this.nextElementSibling;
+
+  this.classList.toggle("active");
+
+  if (this.classList.contains("active")) {
+    dropdownItem.style.maxHeight = dropdownItem.scrollHeight + "px";
+  } else {
+    dropdownItem.style.maxHeight = null;
+  }
+}
+
+btnHeaderMenu.addEventListener("click", toggleMenu);
+btnHeaderDropdown.addEventListener("click", toggleDropdown);
