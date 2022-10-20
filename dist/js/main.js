91c8f4a1c22b9b163e3803d774fa6a0c3fceb4d5
Dejan Nadoba <dejan.nadoba@vega.local>
2022-10-20 16:45:18 +0200

    people page almost done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,17 +1,11 @@
 "use strict";
+import "./header/header.js";
 import "./swiper/swiper.js";
 import "./accordion/accordion.js";
+import "./people/people.js";
 
 const heroSection = document.querySelector(".js-hero");
 const heroScroll = document.querySelector(".js-hero-link");
-
-const header = document.querySelector(".js-header");
-const headerNav = document.querySelector(".js-header-nav");
-const btnHeaderMenu = document.querySelector(".js-header-btn-menu");
-const btnHeaderDropdown = document.querySelector(".js-header-btn-dropdown");
-const btnContact = document.querySelector(".js-btn-contact");
-
-const mobile = innerWidth < 768;
 
 function scrollToSecondSection() {
   const sectionCords = heroSection.getBoundingClientRect();
@@ -23,45 +17,6 @@ function scrollToSecondSection() {
   });
 }
 
-function toggleMenu() {
-  const iconMenu = this.querySelector(".js-header-icon-menu");
-  const iconClose = this.querySelector(".js-header-icon-close");
-
-  iconMenu.classList.toggle("hidden");
-  iconClose.classList.toggle("hidden");
-  this.classList.toggle("active");
-
-  if (this.classList.contains("active")) {
-    if (mobile) {
-      btnContact.style.display = "block";
-    }
-
-    header.classList.add("active");
-    headerNav.style.display = "block";
-  } else {
-    if (mobile) {
-      btnContact.style.display = "none";
-    }
-
-    header.classList.remove("active");
-    headerNav.style.display = "none";
-  }
-}
-
-function toggleDropdown() {
-  const dropdownItem = this.nextElementSibling;
-
-  this.classList.toggle("active");
-
-  if (this.classList.contains("active")) {
-    dropdownItem.style.maxHeight = dropdownItem.scrollHeight + "px";
-  } else {
-    dropdownItem.style.maxHeight = null;
-  }
-}
-
 if (heroScroll) {
   heroScroll.addEventListener("click", scrollToSecondSection);
 }
-btnHeaderMenu.addEventListener("click", toggleMenu);
-btnHeaderDropdown.addEventListener("click", toggleDropdown);
---
+++ src/js/people/people.js
@@ -0,0 +1,25 @@
+const lightbox = document.querySelector(".js-lightbox");
+const members = document.querySelectorAll(".js-people-member");
+const btnClose = document.querySelector(".js-lightbox-btn");
+
+function displayMember() {
+  const member = this;
+  const img = member.querySelector(".js-people-img").src;
+  const name = member.querySelector(".js-people-name").textContent;
+  const title = member.querySelector(".js-people-desc").textContent;
+
+  lightbox.querySelector(".js-lightbox-img").setAttribute("src", img);
+  lightbox.querySelector(".js-lightbox-name").textContent = name;
+  lightbox.querySelector(".js-lightbox-title").textContent = title;
+
+  lightbox.classList.add("shown");
+}
+
+function hideLayout() {
+  lightbox.classList.remove("shown");
+}
+
+members.forEach((member) => member.addEventListener("click", displayMember));
+if (btnClose) {
+  btnClose.addEventListener("click", hideLayout);
+}
